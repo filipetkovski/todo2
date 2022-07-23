5ee30f7b1947cf0f2e53921709cddd0fe41761b7
Filip Petkovski <108181951+filipetkovski@users.noreply.github.com>
2022-07-23 08:23:44 +0200

    Add files via upload

--- JavaScript/nav-menu.js
+++ JavaScript/nav-menu.js
@@ -53,59 +53,36 @@ function menuOpen() {
     const nav1= document.getElementById("nav");
     const ham1 = document.getElementById("ham1");
     const ham2 = document.getElementById("ham2");
-    let navButton = [];
-
-    navButton[0] = document.getElementById("nav1");
-    navButton[1] = document.getElementById("nav2");
-    navButton[2] = document.getElementById("nav3");
-    navButton[3] = document.getElementById("nav4");
-    navButton[4] = document.getElementById("nav5");
 
     const dark = document.getElementById("darkTheme");
-    dark.style.left = "250px";
-    dark.style.right = "0";
+    dark.style.bottom = "0";
     dark.style.opacity = "1";
 
-    nav1.style.width = "250px";
+    nav1.style.height = "100vh"
     nav1.style.boxShadow = "0 5px 10px black";
+    nav1.style.opacity = "1";
 
     ham1.style.display = "none";
     ham2.style.display = "flex";
     ham2.style.justifyContent = "center";
     ham2.style.alignItems = "center";
 
-    for(let i=0;i<5;i++) {
-        navButton[i].style.fontSize = "1em";
-        navButton[i].style.opacity = "1";
-    }
 }
 
 function menuClose() {
     let nav1= document.getElementById("nav");
     let ham1 = document.getElementById("ham1");
     let ham2 = document.getElementById("ham2");
-    let navButton = [];
-
-    navButton[0] = document.getElementById("nav1");
-    navButton[1] = document.getElementById("nav2");
-    navButton[2] = document.getElementById("nav3");
-    navButton[3] = document.getElementById("nav4");
-    navButton[4] = document.getElementById("nav5");
 
     const dark = document.getElementById("darkTheme");
-    dark.style.left = "0";
-    dark.style.right = "unset";
+    dark.style.bottom = "unset";
     dark.style.opacity = "0";
 
-    nav1.style.width = "0";
+    nav1.style.height = "0"
     nav1.style.boxShadow = "none";
+    nav1.style.opacity = "0";
 
     ham1.style.display = "flex";
     ham2.style.display = "none";
-
-    for(let i=0;i<5;i++) {
-        navButton[i].style.fontSize = "0";
-        navButton[i].style.opacity = "0";
-    }
 
 }
